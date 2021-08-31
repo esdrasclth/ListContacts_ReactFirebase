b0e83e46e653be44b86ee7ab8581ba89c9a8a340
Esdras Clother <esdras.clother@outlook.com>
2021-08-31 11:13:36 -0600

    ending the contact list structure

--- src/components/ListaContactos.js
+++ src/components/ListaContactos.js
@@ -1,6 +1,8 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+
 import db from '../firebase/firebaseConfig';
+import Contacto from './Contacto';
 
 const ListaContactos = () => {
 
@@ -17,18 +19,25 @@ const ListaContactos = () => {
         }
     ]);
 
-    return ( 
+    return (
         contactos.length > 0 &&
         <ContenedorContactos>
             {contactos.map((contacto) => {
-                return <p key={contacto.id}>{contacto.nombre} - {contacto.correo}</p>
+                return (
+                    <Contacto
+                        key={contacto.id}
+                        id={contacto.id}
+                        nombre={contacto.nombre}
+                        correo={contacto.correo}
+                    />
+                )
             })}
         </ContenedorContactos>
-     );
+    );
 }
 
 const ContenedorContactos = styled.div`
     margin-top: 40px;
 `;
- 
+
 export default ListaContactos;
